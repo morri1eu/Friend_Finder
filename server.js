1acb0544440ee457be0283e787d6f67f12b7051d
Morri1eu <morri1eu@gmail.com>
2018-07-23 14:54:46 -0700

    fixed port error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser= require('body-parser')
 
 var app= express()
 
-var PORT= process.env.port || 3000
+var PORT= process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
